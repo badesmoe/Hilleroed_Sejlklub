d7f47f49f37f8477ad633eebb4dd0ba3e4f0075f
Christoffer <bendigsunne@hotmail.com>
2025-12-04 10:22:10 +0100

    restructred UserFile and IReporitoryUser. made UserFile implement IRepositoryUser.

---
+++ Hillerød Sejlklub/Repository/Users/IReposioryUser.cs
@@ -0,0 +1,11 @@
+﻿using Hillerød_Sejlklub.Models;
+
+namespace Hillerød_Sejlklub.Repository.Users
+{
+    public interface IReposioryUser
+    {
+        void Add(User user);
+        User? Search(string searchTerm);
+        void Delete(int id);
+    }
+}
