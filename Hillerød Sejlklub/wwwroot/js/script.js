68f94eeb523416534c6ce300edac385727b2cf71
Christoffer <bendigsunne@hotmail.com>
2025-12-11 12:46:52 +0100

    implemented windy api

---
+++ Hillerød Sejlklub/wwwroot/js/script.js
@@ -0,0 +1,21 @@
+﻿const options = {
+    // Required: API key
+    key: '4UlF1UaXPObBXQqpYBkNfgJipoadvIBO', // REPLACE WITH YOUR KEY !!!
+
+    // Put additional console output
+    verbose: true,
+
+    // Optional: Initial state of the map
+    lat: 56,
+    lon: 12.4,
+    zoom: 10,
+};
+
+// Initialize Windy API
+windyInit(options, windyAPI => {
+    // windyAPI is ready, and contain 'map', 'store',
+    // 'picker' and other usefull stuff
+
+    const { map } = windyAPI;
+    // .map is instance of Leaflet map
+});
